b02107b104af54ca59315f16e277f27fbba1eacd
Sapov <sapov@mail.ru>
2023-09-21 00:11:17 +0300

    add city

--- create_exel.py
+++ create_exel.py
@@ -13,6 +13,7 @@ def create_exel():
     ws.cell(row=1, column=2, value='Описание')
     ws.cell(row=1, column=3, value='link')
     ws.cell(row=1, column=4, value='Price')
+    ws.cell(row=1, column=5, value='city')
 
     with open('items.json') as file:
         st = json.load(file)
@@ -23,6 +24,11 @@ def create_exel():
             ws.cell(row=index + 2, column=2, value=st[index]['description'])
             ws.cell(row=index + 2, column=3, value=st[index]['link'])
             ws.cell(row=index + 2, column=4, value=st[index]['price'])
+            ws.cell(row=index + 2, column=5, value=st[index]['city'])
 
     # Save the file
-    wb.save("sample.xlsx")
+    wb.save("result.xlsx")
+
+
+if __name__ == '__main__':
+    create_exel()
